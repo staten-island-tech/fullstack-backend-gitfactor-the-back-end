2ad3f4a44d3d26d6c659352816e12f257bbaa064
popplio2 <danikbrisk@yahoo.com>
2022-02-19 14:33:24 -0500

    added leftValue to model

--- Models/users.js
+++ Models/users.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  leftValue: {
+    type: Number,
+    required: true,
+  },
   lifeCount: {
     type: Number,
     required: true,
